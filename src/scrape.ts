996ee39be9ec4a6a6acd8711e7abc07ce752f2da
sidedoc <info@biobotanics.com.au>
2024-10-09 16:18:09 +1100

    Update scrape.ts

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,6 +1,19 @@
-import { Readability } from "@mozilla/readability";
-import { parseHTML } from "linkedom";
+import { Readability, isProbablyReaderable } from "@mozilla/readability";
+import { JSDOM } from "jsdom";
 import TurndownService from "./turndown";
+import opengraph_scraper from "open-graph-scraper";
+import { getSubtitles } from "youtube-captions-scraper";
+import pdf2md from "@opendocsg/pdf2md";
+
+/**
+ * Interface for Open Graph data.
+ */
+interface OgObject {
+  ogTitle?: string;
+  ogDescription?: string;
+  ogDate?: string;
+  ogImage?: Array<{ url: string }>;
+}
 
 export const scrape = async ({
   url,
@@ -15,8 +28,7 @@ export const scrape = async ({
     },
   });
   const html = await response.text();
-  console.log("html", html);
-  const article = extract(html);
+  const article = await extract(html, new URL(url));
 
   if (article == null) {
     return null;
@@ -33,24 +45,71 @@ export const scrape = async ({
   }
 };
 
-const extract = (html: string) => {
-  var doc = parseHTML(html);
-  let reader = new Readability(doc.window.document);
-  return reader.parse();
+const extract = async (html: string, url: URL) => {
+  const doc = new JSDOM(html, { url: url.href });
+  const document = doc.window.document;
+
+  if (isProbablyReaderable(document, { minContentLength: document.body.textContent.length / 3 })) {
+    let reader = new Readability(document);
+    return reader.parse();
+  } else {
+    const ogData = await parseOg(html);
+    const ogMarkdown = ogToMarkdown(ogData);
+    return { content: ogMarkdown, textContent: document.body.textContent };
+  }
 };
 
 const convertToMarkdown = (html: string) => {
   const turndown = new TurndownService();
-  const doc = parseHTML(html);
+  const doc = new JSDOM(html);
   return turndown.turndown(doc.window.document);
 };
 
-//
 const cleanString = (str: string) =>
   str
-    // Replace various whitespace and zero-width characters with a single space
     .replace(/[\s\t\u200B-\u200D\uFEFF]+/g, " ")
-    // Remove leading whitespace from each line in the string
     .replace(/^\s+/gm, "")
-    // Collapse multiple newline characters into a single newline
     .replace(/\n+/g, "\n");
+
+const parseOg = async (html: string): Promise<OgObject> => {
+  const options = { html, timeout: 10000 };
+  const ret = await opengraph_scraper(options);
+  return ret.result;
+};
+
+const ogToMarkdown = (ogData: OgObject): string => {
+  const { ogTitle, ogDescription, ogImage, ogDate } = ogData;
+  let markdown = "";
+
+  if (ogTitle) markdown += `# ${ogTitle}\n\n`;
+  if (ogDate) {
+    const formattedDate = new Date(ogDate).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric", timeZone: "UTC" });
+    markdown += `*${formattedDate}*\n\n`;
+  }
+  const imgURL = ogImage ? ogImage[0]?.url : undefined;
+  if (imgURL) markdown += `![Thumbnail](${imgURL})\n\n`;
+  if (ogDescription) markdown += `${ogDescription}\n\n`;
+
+  return markdown;
+};
+
+const scrapeableUrl = (url: URL): { url: URL; redirect: "follow" | "manual"; userAgent: string } => {
+  const twitter = /^(?:.*\.)?(twitter\.com|x\.com)$/i;
+  const reddit = /^(?:.*\.)?(www\.)?(reddit\.com|redd\.it)$/i;
+  const defaults = { userAgent: "Googlebot/2.1 (+http://www.google.com/bot.html)" };
+
+  if (twitter.test(url.host)) {
+    url.host = "fxtwitter.com";
+    return { ...defaults, url, redirect: "manual" };
+  } else if (reddit.test(url.host)) {
+    url.host = "old.reddit.com";
+    return { ...defaults, url, redirect: "manual" };
+  }
+  return { ...defaults, url, redirect: "follow" };
+};
+
+const getYoutubeVideoID = (url: URL): string | null => {
+  const regExp = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/i;
+  const match = url.href.match(regExp);
+  return match ? match[1] : null;
+};
